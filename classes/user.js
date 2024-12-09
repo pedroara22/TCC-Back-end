209c7c46c374e948ff7e7eb374b6d22750b308aa
Pedro Araújo <127028443+pedroara22@users.noreply.github.com>
2024-12-08 21:40:25 -0300

    Update user.js

--- classes/user.js
+++ classes/user.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const user = mongoose.Schema({
         name: String,
         email: String,
-        password: String
+        password: String,
+	score: Number
 });
 
 module.exports = mongoose.model('User', user);
